c7490d24925ad3fd01477402d9a17efeab919369
Pablo Stebler <pablo@stebler.xyz>
2022-02-05 01:11:47 +0100

    Allow closing the ghost replay pause menu with option
    Closes #100.

--- payload/game/ui/page/RaceMenuPage.c
+++ payload/game/ui/page/RaceMenuPage.c
@@ -1,5 +1,13 @@
 #include "RaceMenuPage.h"
 
+bool GhostReplayPauseMenuPage_vf_74;
+
+static bool my_GhostReplayPauseMenuPage_vf_74(void) {
+    // Allow closing with option
+    return true;
+}
+PATCH_B(GhostReplayPauseMenuPage_vf_74, my_GhostReplayPauseMenuPage_vf_74);
+
 static u32 afterTaButtons[] = {
     0x3,
     0x5,
