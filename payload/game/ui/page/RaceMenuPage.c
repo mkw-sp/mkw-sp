89922f1b6db660ef392e9a9574215fa6bec5e92e
Pablo Stebler <pablo@stebler.xyz>
2022-02-06 21:46:43 +0100

    Add button to change ghosts in time trials

--- payload/game/ui/page/RaceMenuPage.c
+++ payload/game/ui/page/RaceMenuPage.c
@@ -1,36 +1,59 @@
 #include "RaceMenuPage.h"
 
-bool GhostReplayPauseMenuPage_vf_74;
+static const u32 ghostWatchPauseButtons[] = {
+    0xc,
+    0xf,
+    0xe,
+    0x12,
+    0xd,
+};
 
-static bool my_GhostReplayPauseMenuPage_vf_74(void) {
+u32 GhostWatchPauseMenuPage_getButtonCount(void);
+
+static u32 my_GhostWatchPauseMenuPage_getButtonCount(void) {
+    return ARRAY_SIZE(ghostWatchPauseButtons);
+}
+PATCH_B(GhostWatchPauseMenuPage_getButtonCount, my_GhostWatchPauseMenuPage_getButtonCount);
+
+const u32 *GhostWatchPauseMenuPage_getButtons(void);
+
+static const u32 *my_GhostWatchPauseMenuPage_getButtons(void) {
+    return ghostWatchPauseButtons;
+}
+PATCH_B(GhostWatchPauseMenuPage_getButtons, my_GhostWatchPauseMenuPage_getButtons);
+
+bool GhostWatchPauseMenuPage_vf_74(void);
+
+static bool my_GhostWatchPauseMenuPage_vf_74(void) {
     // Allow closing with option
     return true;
 }
-PATCH_B(GhostReplayPauseMenuPage_vf_74, my_GhostReplayPauseMenuPage_vf_74);
+PATCH_B(GhostWatchPauseMenuPage_vf_74, my_GhostWatchPauseMenuPage_vf_74);
 
-static u32 afterTaButtons[] = {
+static const u32 afterTaButtons[] = {
     0x3,
+    0x12,
     0x5,
     0x6,
     0x4,
     0x1,
 };
 
-u32 AfterTAMenuPage_getButtonCount(void);
+u32 AfterTimeAttackMenuPage_getButtonCount(void);
 
-static u32 my_AfterTAMenuPage_getButtonCount(void) {
+static u32 my_AfterTimeAttackMenuPage_getButtonCount(void) {
     return ARRAY_SIZE(afterTaButtons);
 }
-PATCH_B(AfterTAMenuPage_getButtonCount, my_AfterTAMenuPage_getButtonCount);
+PATCH_B(AfterTimeAttackMenuPage_getButtonCount, my_AfterTimeAttackMenuPage_getButtonCount);
 
-const u32 *AfterTAMenuPage_getButtons(void);
+const u32 *AfterTimeAttackMenuPage_getButtons(void);
 
-static const u32 *my_AfterTAMenuPage_getButtons(void) {
+static const u32 *my_AfterTimeAttackMenuPage_getButtons(void) {
     return afterTaButtons;
 }
-PATCH_B(AfterTAMenuPage_getButtons, my_AfterTAMenuPage_getButtons);
+PATCH_B(AfterTimeAttackMenuPage_getButtons, my_AfterTimeAttackMenuPage_getButtons);
 
-static u32 vsPauseButtons[] = {
+static const u32 vsPauseButtons[] = {
     0x0,
     0x2,
     0x1,
@@ -57,7 +80,7 @@ static const char *my_VsPauseMenuPage_getFile(void) {
 }
 PATCH_B(VsPauseMenuPage_getFile, my_VsPauseMenuPage_getFile);
 
-static u32 battlePauseButtons[] = {
+static const u32 battlePauseButtons[] = {
     0x0,
     0x2,
     0x1,
@@ -83,3 +106,26 @@ static const char *my_BattlePauseMenuPage_getFile(void) {
     return "PauseMenuVS";
 }
 PATCH_B(BattlePauseMenuPage_getFile, my_BattlePauseMenuPage_getFile);
+
+static const u32 timeAttackPauseButtons[] = {
+    0x0,
+    0x2,
+    0x12,
+    0x5,
+    0x6,
+    0x1,
+};
+
+u32 TimeAttackPauseMenuPage_getButtonCount(void);
+
+static u32 my_TimeAttackPauseMenuPage_getButtonCount(void) {
+    return ARRAY_SIZE(timeAttackPauseButtons);
+}
+PATCH_B(TimeAttackPauseMenuPage_getButtonCount, my_TimeAttackPauseMenuPage_getButtonCount);
+
+const u32 *TimeAttackPauseMenuPage_getButtons(void);
+
+static const u32 *my_TimeAttackPauseMenuPage_getButtons(void) {
+    return timeAttackPauseButtons;
+}
+PATCH_B(TimeAttackPauseMenuPage_getButtons, my_TimeAttackPauseMenuPage_getButtons);
