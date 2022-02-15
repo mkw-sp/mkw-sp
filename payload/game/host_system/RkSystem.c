f21d3548eaf77bdd9cf52a10e108c80c2e7e1707
Star <65995358+StarMKWii@users.noreply.github.com>
2022-02-15 01:49:46 -0500

    Automatically display exception information on the television screen when an exception occurs

--- payload/game/host_system/RkSystem.c
+++ payload/game/host_system/RkSystem.c
@@ -3,6 +3,10 @@
 extern void RkSystem_initialize;
 extern void RkSystem_run;
 
+// Automatically display exception information on the television screen when an exception occurs
+PATCH_NOP(RkSystem_initialize, 0x1A0);
+PATCH_U32(RkSystem_initialize, 0x1A4, 0x38600000 /* li r3, 0 */);
+
 // Don't create a heap for the rel because we already allocate some space from the arena for it. The
 // function we are patching is still RkSystem_initialize but the NTSC-K offset is different so as a
 // workaround we reference it from the next function instead.
