b7171ee21fbf9b01a0bbd747aec6615ffb083cf5
Star <65995358+StarMKWii@users.noreply.github.com>
2022-02-15 12:09:23 -0500

    Add Exception Console (#167)
    * Revert from using the default error handler
    
    * Display all exception information when an exception occurs
    
    * Automatically display exception information on the television screen when an exception occurs
    
    * Increase the number of lines that the exception console can hold
    
    * Moved assembly patches to their own files
    
    * Merge consecutive assembly patches into one assembly patch

--- payload/game/host_system/RkSystem.c
+++ payload/game/host_system/RkSystem.c
@@ -1,10 +1,6 @@
 #include <Common.h>
 
-extern void RkSystem_initialize;
 extern void RkSystem_run;
-
-// Use the exception handler from the SDK
-PATCH_NOP(RkSystem_initialize, 0x19c);
 
 // Don't create a heap for the rel because we already allocate some space from the arena for it. The
 // function we are patching is still RkSystem_initialize but the NTSC-K offset is different so as a
