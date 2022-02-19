9d969d2560cb108b3a19b447684049a7cd36f398
Star <65995358+StarMKWii@users.noreply.github.com>
2022-02-19 02:50:15 -0500

    Complete the section layouts of the '.dol' and '.rel' modules for all regions

--- include/Dol.h
+++ include/Dol.h
@@ -2,17 +2,17 @@
 
 #include <Common.h>
 
-extern void dol_start;
-extern void dol_end;
+extern void dol_init_start;
+extern void dol_sbss2_end;
 
 static void *Dol_getStart(void) {
-    return &dol_start;
+    return &dol_init_start;
 }
 
 static void *Dol_getEnd(void) {
-    return &dol_end;
+    return &dol_sbss2_end;
 }
 
 static u32 Dol_getSize(void) {
-    return &dol_end - &dol_start;
+    return &dol_sbss2_end - &dol_init_start;
 }
