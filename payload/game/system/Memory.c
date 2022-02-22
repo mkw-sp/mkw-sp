35d77aaf0876399a55a6a2fe7c43d441f8f82251
Star <65995358+StarMKWii@users.noreply.github.com>
2022-02-22 09:54:35 -0500

    Replace missed guard clause with assertation

--- payload/game/system/Memory.c
+++ payload/game/system/Memory.c
@@ -12,8 +12,7 @@ void ProtectRangeModule(u32 channel, void* start, void* end, u32 permissions)
     u32 start_address = MemoryRoundUp1024B((uintptr_t)start);
     u32 end_address   = MemoryRoundDown1024B((uintptr_t)end);
 
-    if (end_address <= start_address)
-	return;
+    assert(!(end_address <= start_address));
 
     OSReport("[MEM] OSProtectRange: 0x%08X - 0x%08X [CHAN: %u | PERMS: %c%c]\n", start_address, end_address, channel,
                                                                                  (permissions & OS_PROTECT_PERMISSION_READ ) ? 'r' : '-',
