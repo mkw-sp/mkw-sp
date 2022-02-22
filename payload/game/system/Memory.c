53923c1e1ff0a0c013e5fc3114e2a9d4856cc555
Star <65995358+StarMKWii@users.noreply.github.com>
2022-02-22 09:39:59 -0500

    Call the 'OSReport' function before calling the 'OSProtectRange' function

--- payload/game/system/Memory.c
+++ payload/game/system/Memory.c
@@ -15,8 +15,8 @@ void ProtectRangeModule(u32 channel, void* start, void* end, u32 permissions)
     if (end_address <= start_address)
 	return;
 
-    OSProtectRange(channel, (void*)start_address, end_address - start_address, permissions);
     OSReport("[MEM] OSProtectRange: 0x%08X - 0x%08X [CHAN: %u | PERMS: %c%c]\n", start_address, end_address, channel,
                                                                                  (permissions & OS_PROTECT_PERMISSION_READ ) ? 'r' : '-',
 										 (permissions & OS_PROTECT_PERMISSION_WRITE) ? 'w' : '-');
+    OSProtectRange(channel, (void*)start_address, end_address - start_address, permissions);
 }
