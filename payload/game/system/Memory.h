b2e6dde28f092bffc33995753b35a58bd0ab9b7d
Star <65995358+StarMKWii@users.noreply.github.com>
2022-02-24 16:24:05 -0500

    Correct flaky memory preprocessor directives

--- payload/game/system/Memory.h
+++ payload/game/system/Memory.h
@@ -4,7 +4,7 @@
 #include <revolution/os/OSMemory.h>
 
 // Rounding
-#define MemoryRoundDown1024B(address) ((u32)address & ~0x3FF)
-#define MemoryRoundUp1024B(address) (((u32)address + 0x3FF) & ~0x3FF)
+#define MemoryRoundDown1024B(address) (((u32)(address)) & ~0x3FF)
+#define MemoryRoundUp1024B(address) (((u32)(address) + 0x3FF) & ~0x3FF)
 
 void ProtectRangeModule(u32 channel, void* start, void* end, u32 permissions);
