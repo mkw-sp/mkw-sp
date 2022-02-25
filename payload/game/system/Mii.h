59ad0f3fe1789819e34043b33105e8eb8362e6c6
Pablo Stebler <pablo@stebler.xyz>
2022-02-25 01:51:57 +0100

    Use Storage interface for ghost loading/saving

--- payload/game/system/Mii.h
+++ payload/game/system/Mii.h
@@ -9,7 +9,8 @@ typedef struct {
 static_assert(sizeof(MiiId) == 0x8);
 
 typedef struct {
-    u8 _00[0x4a - 0x00];
+    u8 _00[0x36 - 0x00];
+    wchar_t name[10];
     u16 crc16;
 } RawMii;
 static_assert(sizeof(RawMii) == 0x4c);
--- payload/sp/Storage.h
+++ payload/sp/Storage.h
@@ -8,30 +8,48 @@ typedef struct {
 
 typedef struct {
     Node;
-} Dir;
+} File;
 
 typedef struct {
     Node;
-} File;
+} Dir;
 
 enum {
-    MODE_READ = 1 << 0,
-    MODE_WRITE = 1 << 1,
+    NODE_TYPE_NONE = 0x0,
+    NODE_TYPE_FILE = 0x1,
+    NODE_TYPE_DIR = 0x2,
 };
 
 typedef struct {
-    bool (*open)(File *file, const char *path, u32 mode);
+    wchar_t name[255 + 1];
+    u32 type;
+} DirEntry;
+
+// TODO this is a bit confusing
+enum {
+    MODE_READ = 1 << 0,
+    MODE_WRITE = 1 << 1,
+    MODE_CREATE_NEW = 1 << 2,
+    MODE_CREATE_ALWAYS = 1 << 3,
+};
+
+typedef struct {
+    bool (*open)(File *file, const wchar_t *path, u32 mode);
     bool (*close)(File *file);
     bool (*read)(File *file, void *dst, u32 size, u32 *readSize);
     bool (*write)(File *file, const void *src, u32 size, u32 *writtenSize);
     u64 (*size)(File *file);
     bool (*lseek)(File *file, u64 offset);
     u64 (*tell)(File *file);
+    bool (*createDir)(const wchar_t *path, bool overwrite);
+    bool (*openDir)(Dir *dir, const wchar_t *path);
+    bool (*readDir)(Dir *dir, DirEntry *entry);
+    bool (*closeDir)(Dir *dir);
 } Storage;
 
 bool Storage_init(void);
 
-bool Storage_open(File *file, const char *path, u32 mode);
+bool Storage_open(File *file, const wchar_t *path, u32 mode);
 
 bool Storage_close(File *file);
 
@@ -44,3 +62,17 @@ u64 Storage_size(File *file);
 bool Storage_lseek(File *file, u64 offset);
 
 u64 Storage_tell(File *file);
+
+bool Storage_readFile(const wchar_t *path, void *dst, u32 size, u32 *readSize);
+
+bool Storage_writeFile(const wchar_t *path, bool overwrite, const void *src, u32 size);
+
+bool Storage_createDir(const wchar_t *path, bool overwrite);
+
+bool Storage_openDir(Dir *dir, const wchar_t *path);
+
+bool Storage_readDir(Dir *dir, DirEntry *entry);
+
+bool Storage_closeDir(Dir *dir);
+
+u32 Storage_type(const wchar_t *path);
