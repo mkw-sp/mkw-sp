4094a3b3df5360bfa31ffdc6ba42ec3057386104
riidefi <34194588+riidefi@users.noreply.github.com>
2022-03-03 18:00:50 -0700

    Add CT crash handler for too many BREFF files

--- payload/game/system/BugCheck.c
+++ payload/game/system/BugCheck.c
@@ -116,28 +116,6 @@ void InvalidTexRefFail(const char *mdl0_name) {
     SpBugCheck(file, desc);
 }
 
-// Based on https://github.com/llvm/llvm-project/blob/main/libc/src/string/strstr.cpp#L18
-static char *llvm_strstr(const char *haystack, const char *needle) {
-    for (size_t i = 0; haystack[i]; ++i) {
-        size_t j;
-        for (j = 0; haystack[i + j] && haystack[i + j] == needle[j]; ++j)
-            ;
-        if (!needle[j])
-            return (char *)(haystack + i);
-    }
-    return NULL;
-}
-
-// Many ugly crashes caused by this function. Ideally, we'll catch them before these
-// asserts trigger.
-static char *my_strstr(char *string, char *substring) {
-    assert(string);
-    assert(substring);
-
-    return llvm_strstr(string, substring);
-}
-PATCH_B(strstr, my_strstr);
-
 // XREF: eggEffectCreator.S
 void MissingBreffFail(const char *breff_name) {
     char file[65];
@@ -176,4 +154,42 @@ void InvalidRevisionFail(
     }
 
     SpBugCheck(file, desc);
+}
+
+static char loadedEffects[256];
+
+// XREF: ObjEffect.S
+void LogLoadedEffect(int index, const char *path) {
+    const char *trailing = strrchr(path, '/');
+    if (trailing != NULL)
+        path = trailing + 1;
+
+    if (loadedEffects[0] == '\0') {
+        snprintf(loadedEffects, sizeof(loadedEffects), "%i: %s", index, path);
+        return;
+    }
+
+    char loaded_effects_tmp[sizeof(loadedEffects)];
+    int res = snprintf(loaded_effects_tmp, sizeof(loaded_effects_tmp), "%s, %i: %s", loadedEffects,
+            index, path);
+    if (res <= 0) {
+        return;
+    }
+
+    snprintf(loadedEffects, sizeof(loadedEffects), "%s", loaded_effects_tmp);
+}
+
+// XREF: ObjEffect.S
+void ResetLoadedEffects(void) {
+    memset(loadedEffects, 0, sizeof(loadedEffects));
+}
+
+// XREF: ObjEffect.S
+void TooManyEffectsFail(int current_count, int capacity, const char *path) {
+    char desc[512];
+    snprintf(desc, sizeof(desc),
+            "Too many BREFF (particle effect) files. Maximum is %i.\n\nEffects (%i): %s.",
+            capacity, current_count, loadedEffects[0] != '\0' ? loadedEffects : "?");
+
+    SpBugCheck(path, desc);
 }
