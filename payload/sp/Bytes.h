401ab404de90afac076ed100b75b2ae5ad62faa3
riidefi <34194588+riidefi@users.noreply.github.com>
2022-03-06 20:41:58 -0700

    NetStorageClient: Support compiling on PC

--- payload/sp/Bytes.h
+++ payload/sp/Bytes.h
@@ -2,6 +2,70 @@
 
 #include <Common.h>
 
+static inline u32 swap32(u32 v);
+static inline u16 swap16(u16 v);
+
+#define _BSWAP_32(v)                                                                \
+    (((v & 0xff000000) >> 24) | ((v & 0x00ff0000) >> 8) | ((v & 0x0000ff00) << 8) | \
+            ((v & 0x000000ff) << 24))
+#define _BSWAP_16(v) (((v & 0xff00) >> 8) | ((v & 0x00ff) << 8))
+
+#if defined(__llvm__) || (defined(__GNUC__) && !defined(__ICC))
+static inline u32 swap32(u32 v) {
+    return __builtin_bswap32(v);
+}
+static inline u16 swap16(u16 v) {
+    return _BSWAP_16(v);
+}
+#elif defined(_MSC_VER)
+#include <stdlib.h>
+static inline u32 swap32(u32 v) {
+    return _byteswap_ulong(v);
+}
+static inline u16 swap16(u16 v) {
+    return _byteswap_ushort(v);
+}
+#else
+static inline u32 swap32(u32 v) {
+    return _BSWAP_32(v);
+}
+static inline u16 swap16(u16 v) {
+    return _BSWAP_16(v);
+}
+#endif
+
+#if kEndianCurrent == kEndianLittle
+#include <string.h>
+
+// Respects strong aliasing, will never double evaluate
+#define sp_htonl(x)                              \
+    ({                                           \
+        __typeof__(x) _x = x;                    \
+        u32 _u;                                  \
+        static_assert(sizeof(_x) == sizeof(_u)); \
+        memcpy(&_u, &_x, sizeof(_x));            \
+        _u = swap32(_u);                         \
+        memcpy(&_x, &_u, sizeof(_x));            \
+        _x;                                      \
+    })
+#define sp_htons(x)                              \
+    ({                                           \
+        __typeof__(x) _x = x;                    \
+        u16 _u;                                  \
+        static_assert(sizeof(_x) == sizeof(_u)); \
+        memcpy(&_u, &_x, sizeof(_x));            \
+        _u = swap16(_u);                         \
+        memcpy(&_x, &_u, sizeof(_x));            \
+        _x;                                      \
+    })
+#else
+#define sp_htonl(x) (x)
+#define sp_htons(x) (x)
+#endif
+
+#define sp_ntohl(x) sp_htonl(x)
+#define sp_ntohs(x) sp_htons(x)
+
 static u16 read_u16(const u8 *data, u32 offset) {
     const u8 *base = data + offset;
     return base[0x0] << 8 | base[0x1];
