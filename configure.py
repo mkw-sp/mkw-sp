acc168851a969aadb7fcdf87e9c0ce3441e4e1b5
Star <65995358+StarMKWii@users.noreply.github.com>
2022-03-15 11:48:42 -0400

    Disable code execution in the MEM2 region
    These changes will have no effect until the patcher runs before the '.dol' module.

--- configure.py
+++ configure.py
@@ -281,6 +281,7 @@ code_in_files = {
         os.path.join('payload', 'revolution', 'dvdex.c'),
         os.path.join('payload', 'revolution', 'nand.c'),
         os.path.join('payload', 'revolution', 'os', 'OSError.c'),
+        os.path.join('payload', 'revolution', 'os', 'OSMemory.S'),
         os.path.join('payload', 'revolution', 'os', 'OSThread.S'),
         os.path.join('payload', 'revolution', 'os', 'OSThread.c'),
         os.path.join('payload', 'sp', 'Fatal.c'),
