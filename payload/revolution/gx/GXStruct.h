6fb2d024e3d3184894cd1d287c462189b73cc21c
riidefi <34194588+riidefi@users.noreply.github.com>
2022-04-04 11:56:14 -0600

    Add GlyphRenderer, RKFontGlyphQuad, etc to Font.h

--- payload/revolution/gx/GXStruct.h
+++ payload/revolution/gx/GXStruct.h
@@ -11,18 +11,22 @@ typedef struct {
 static_assert(sizeof(GXColor) == 0x4);
 
 typedef struct GXRenderModeObj {
-  int tv_mode;
-  u16 fb_width;
-  u16 efb_height;
-  u16 xfb_height;
-  u16 vi_x;
-  u16 vi_y;
-  u16 vi_width;
-  u16 vi_height;
-  int vi_xfb;
-  u8 field;
-  u8 aa;
-  u8 sample[12][2];
-  u8 vert_filter[7];
+    int tv_mode;
+    u16 fb_width;
+    u16 efb_height;
+    u16 xfb_height;
+    u16 vi_x;
+    u16 vi_y;
+    u16 vi_width;
+    u16 vi_height;
+    int vi_xfb;
+    u8 field;
+    u8 aa;
+    u8 sample[12][2];
+    u8 vert_filter[7];
 } GXRenderModeObj;
 static_assert(sizeof(GXRenderModeObj) == 0x3c);
+
+typedef struct {
+    u8 _00[0x20 - 0x00];
+} GXTexObj;
