28c97ec49d8f4e5b5f728c9506d31f4195154d14
Pablo Stebler <pablo@stebler.xyz>
2022-04-13 23:14:43 +0200

    Add links to SD documentation

--- payload/sp/Sdi.c
+++ payload/sp/Sdi.c
@@ -1,3 +1,9 @@
+// Resources:
+// - https://wiibrew.org/wiki//dev/sdio
+// - https://github.com/dolphin-emu/dolphin/blob/master/Source/Core/Core/IOS/SDIO/SDIOSlot0.cpp
+// - https://www.sdcard.org/cms/wp-content/themes/sdcard-org/dl.php?f=Part1_Physical_Layer_Simplified_Specification_Ver8.00.pdf
+// - https://github.com/devkitPro/libogc/blob/master/libogc/wiisd.c
+
 #include "Sdi.h"
 
 #include <revolution.h>
