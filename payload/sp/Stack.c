3a7ed06df50ba80f9b6430c0020d49ec6eeaf5be
Star <65995358+StarMKWii@users.noreply.github.com>
2022-04-13 18:00:00 -0400

    Refactor incongruous search functions

--- payload/sp/Stack.c
+++ payload/sp/Stack.c
@@ -3,7 +3,7 @@
 #include <revolution/os.h>
 #include <sp/Version.h>
 
-typedef bool (*FindFunction)(const u32* start_address, const u32* end_address);
+typedef bool (*FindFunction)(const u32* start_address);
 
 extern void Stack_XORLinkRegisterRestore(void);
 extern void Stack_XORLinkRegisterSave(void);
@@ -29,24 +29,11 @@ static u32 Stack_CreateBranchLinkInstruction(const u32* source_address, const u3
     return (18 << 26) | (((u32)destination_address - (u32)source_address) & 0x3FFFFFC) | (1 << 0);
 }
 
-static u32* Stack_FindLastFunction(u32* start_address, u32* end_address, FindFunction find)
+static u32* Stack_FindFirstFunction(u32* start_address, u32* end_address, FindFunction find, size_t find_instruction_count)
 {
-    while (end_address >= start_address)
+    while (start_address + find_instruction_count <= end_address)
     {
-        end_address--;
-
-        if (find(start_address, end_address))
-            return end_address;
-    }
-
-    return NULL;
-}
-
-static u32* Stack_FindNextFunction(u32* start_address, u32* end_address, FindFunction find)
-{
-    while (start_address < end_address)
-    {
-        if (find(start_address, end_address))
+        if (find(start_address))
             return start_address;
 
         start_address++;
@@ -55,35 +42,48 @@ static u32* Stack_FindNextFunction(u32* start_address, u32* end_address, FindFun
     return NULL;
 }
 
-static bool Stack_IsFunctionEpilogue(const u32* start_address, const u32* end_address)
+static u32* Stack_FindLastFunction(u32* start_address, u32* end_address, FindFunction find, size_t find_instruction_count)
+{
+    while (end_address - find_instruction_count >= start_address)
+    {
+        if (find(end_address - find_instruction_count))
+            return end_address - find_instruction_count;
+
+        end_address--;
+    }
+
+    return NULL;
+}
+
+static bool Stack_IsFunctionEpilogue(const u32* start_address)
 {
     const u32 mtlr = 0x7C0803A6;
     const u16 addi = 0x3821;
     const u32 blr  = 0x4E800020;
 
-    return (end_address - start_address) >= 3 && start_address[0] == mtlr && ((start_address[1] >> 16) == addi) && start_address[2] == blr;
+    return start_address[0] == mtlr && ((start_address[1] >> 16) == addi) && start_address[2] == blr;
 }
 
-static bool Stack_IsFunctionPrologue(const u32* start_address, const u32* end_address)
+static bool Stack_IsFunctionPrologue(const u32* start_address)
 {
     const u16 stwu = 0x9421;
     const u32 mflr = 0x7C0802A6;
 
-    return (end_address - start_address) >= 2 && ((start_address[0] >> 16) == stwu) && start_address[1] == mflr;
+    return ((start_address[0] >> 16) == stwu) && start_address[1] == mflr;
 }
 
-static bool Stack_IsLinkRegisterRestoreInstruction(const u32* start_address, const u32* end_address)
+static bool Stack_IsLinkRegisterRestoreInstruction(const u32* start_address)
 {
     const u16 lr_restore_instruction_high = 0x8001;
 
-    return (end_address - start_address) >= 1 && (end_address[0] >> 16) == lr_restore_instruction_high;
+    return (start_address[0] >> 16) == lr_restore_instruction_high;
 }
 
-static bool Stack_IsLinkRegisterSaveInstruction(const u32* start_address, const u32* end_address)
+static bool Stack_IsLinkRegisterSaveInstruction(const u32* start_address)
 {
     const u16 lr_save_instruction_high = 0x9001;
 
-    return (end_address - start_address) >= 1 && (start_address[0] >> 16) == lr_save_instruction_high;
+    return (start_address[0] >> 16) == lr_save_instruction_high;
 }
 
 void Stack_DoLinkRegisterPatches(u32* start_address, u32* end_address)
@@ -93,35 +93,24 @@ void Stack_DoLinkRegisterPatches(u32* start_address, u32* end_address)
 
     while (start_address < end_address)
     {
-        /*
-         * stwu r1, -0xXXXX(r1)
-         * mflr r0
-         */
-        u32* stwu = Stack_FindNextFunction(start_address, end_address, Stack_IsFunctionPrologue);
+        u32* stwu = Stack_FindFirstFunction(start_address, end_address, Stack_IsFunctionPrologue, 2);
         if (!stwu)
             break;
 
-        /*
-         * mtlr r0
-         * addi r1, r1, 0xXXXX(r1)
-         * blr
-         */
-        u32* mtlr = Stack_FindNextFunction(stwu + 2, end_address, Stack_IsFunctionEpilogue);
+        u32* mtlr = Stack_FindFirstFunction(stwu + 2, end_address, Stack_IsFunctionEpilogue, 3);
         if (!mtlr)
             break;
 
-        // stw r0, 0xXXXX(r1)
-        u32* stw = Stack_FindNextFunction(stwu + 2, mtlr, Stack_IsLinkRegisterSaveInstruction);
+        u32* stw = Stack_FindFirstFunction(stwu + 2, mtlr, Stack_IsLinkRegisterSaveInstruction, 1);
         if (!stw)
             goto label_check_next_function;
 
-        // lwz r0, 0xXXXX(r1)
-        u32* lwz = Stack_FindLastFunction(stw, mtlr, Stack_IsLinkRegisterRestoreInstruction);
+        u32* lwz = Stack_FindLastFunction(stw, mtlr, Stack_IsLinkRegisterRestoreInstruction, 1);
         if (!lwz)
             goto label_check_next_function;
 
-        // If the function does not return, skip over it
-        u32* next_stwu = Stack_FindNextFunction(stwu + 2, end_address, Stack_IsFunctionPrologue);
+        // Skip over functions that do not return
+        u32* next_stwu = Stack_FindFirstFunction(stwu + 2, end_address, Stack_IsFunctionPrologue, 2);
         if (next_stwu && !(next_stwu > mtlr))
         {
             start_address = next_stwu;
