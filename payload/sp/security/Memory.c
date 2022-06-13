7c71f06dd7833cc360451f9938578d1e11c4f180
Star <65995358+StarMKWii@users.noreply.github.com>
2022-06-13 12:00:00 -0400

    Declare variables containing memory addresses as pointers

--- payload/sp/security/Memory.c
+++ payload/sp/security/Memory.c
@@ -8,13 +8,13 @@ void Memory_ProtectRangeModule(u32 channel, void* start, void* end, u32 permissi
     assert(!(channel > OS_PROTECT_CHANNEL_3));
     assert(!(permissions & ~OS_PROTECT_PERMISSION_RW));
 
-    u32 start_address = MemoryRoundUp1024B((uintptr_t)start);
-    u32 end_address   = MemoryRoundDown1024B((uintptr_t)end);
+    u32* start_address = (u32*)MemoryRoundUp1024B(start);
+    u32* end_address = (u32*)MemoryRoundDown1024B(end);
 
     assert(!(end_address <= start_address));
 
     OSReport("[MEM] OSProtectRange: 0x%08X - 0x%08X [CHAN: %u | PERMS: %c%c]\n", start_address, end_address, channel,
                                                                                  (permissions & OS_PROTECT_PERMISSION_READ ) ? 'r' : '-',
 										 (permissions & OS_PROTECT_PERMISSION_WRITE) ? 'w' : '-');
-    OSProtectRange(channel, (void*)start_address, end_address - start_address, permissions);
+    OSProtectRange(channel, start_address, (u32)end_address - (u32)start_address, permissions);
 }
