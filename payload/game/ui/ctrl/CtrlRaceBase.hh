a2d2dc4803fc77a6952937fd852929ba4fb8ab73
Pablo Stebler <pablo@stebler.xyz>
2022-07-07 23:50:55 +0200

    Mute the position tracker if it's hidden

--- payload/game/ui/ctrl/CtrlRaceBase.hh
+++ payload/game/ui/ctrl/CtrlRaceBase.hh
@@ -16,7 +16,7 @@ protected:
     virtual void process();
     virtual void vf_40();
     virtual void vf_44();
-    virtual void vf_48();
+    virtual bool vf_48();
     virtual void vf_4c();
 
     void setPaneColor(const char *pane, bool teamColors);
--- payload/game/ui/ctrl/CtrlRaceRankNum.hh
+++ payload/game/ui/ctrl/CtrlRaceRankNum.hh
@@ -8,8 +8,12 @@ class CtrlRaceRankNum : public CtrlRaceBase {
 public:
     void REPLACED(calcSelf)();
     REPLACE void calcSelf() override;
+    bool REPLACED(vf_48)();
+    REPLACE bool vf_48() override;
 
 private:
+    bool isDisabled() const;
+
     u8 _198[0x1a4 - 0x198];
 };
 static_assert(sizeof(CtrlRaceRankNum) == 0x1a4);
