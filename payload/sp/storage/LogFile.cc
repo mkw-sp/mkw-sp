437ccbb76e131e4adcb7a82e16695bfbc3f51d62
Star <65995358+StarMKWii@users.noreply.github.com>
2022-08-23 10:02:00 -0400

    Move all instances of the 'define' preprocessor directive in the file 'LogFile.h' to the file 'LogFile.cc'

--- payload/sp/storage/LogFile.cc
+++ payload/sp/storage/LogFile.cc
@@ -9,6 +9,11 @@ extern "C" {
 #include <cwctype>
 #include <string_view>
 
+#define LOG_FILE_DIRECTORY L"/mkw-sp/logs"
+#define LOG_FILE_EXTENSION L".log"
+#define LOG_FILE_EXTENSION_LENGTH (sizeof(LOG_FILE_EXTENSION) / sizeof(wchar_t) - 1)
+#define LOG_FILE_NAME_LENGTH 23
+
 namespace SP::LogFile {
 
 static const size_t BUFFER_SIZE = 4096;
