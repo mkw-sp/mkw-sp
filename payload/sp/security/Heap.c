6cf7828398a91f62825f96303ec2427d1fa31079
MikeIsAStar <99037623+MikeIsAStar@users.noreply.github.com>
2022-08-27 04:30:00 +0000

    Enclose macro with parentheses

--- payload/sp/security/Heap.c
+++ payload/sp/security/Heap.c
@@ -3,7 +3,7 @@
 #include <revolution.h>
 #include <vendor/libhydrogen/hydrogen.h>
 
-#define RANDOMIZATION_SPACE_BYTES 1024 * 8
+#define RANDOMIZATION_SPACE_BYTES (1024 * 8)
 
 void Heap_RandomizeMEM1Heaps(void) {
     OSAllocFromMEM1ArenaLo(hydro_random_uniform(RANDOMIZATION_SPACE_BYTES), 32);
