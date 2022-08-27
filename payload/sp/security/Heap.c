a021de8637f23200a37c53333f2b37692da471da
MikeIsAStar <99037623+MikeIsAStar@users.noreply.github.com>
2022-08-27 16:30:00 +0000

    Use a consistent amount of memory for heap randomization

--- payload/sp/security/Heap.c
+++ payload/sp/security/Heap.c
@@ -3,14 +3,16 @@
 #include <revolution.h>
 #include <vendor/libhydrogen/hydrogen.h>
 
-#define RANDOMIZATION_SPACE_BYTES (1024 * 8)
+#define RANDOMIZATION_SPACE_BYTES (1024 * 16)
 
 void Heap_RandomizeMEM1Heaps(void) {
-    OSAllocFromMEM1ArenaLo(hydro_random_uniform(RANDOMIZATION_SPACE_BYTES), 32);
-    OSAllocFromMEM1ArenaHi(hydro_random_uniform(RANDOMIZATION_SPACE_BYTES), 32);
+    const u32 random = hydro_random_uniform(RANDOMIZATION_SPACE_BYTES);
+    OSAllocFromMEM1ArenaLo(random, 32);
+    OSAllocFromMEM1ArenaHi(RANDOMIZATION_SPACE_BYTES - random, 32);
 }
 
 void Heap_RandomizeMEM2Heaps(void) {
-    OSAllocFromMEM2ArenaLo(hydro_random_uniform(RANDOMIZATION_SPACE_BYTES), 32);
-    OSAllocFromMEM2ArenaHi(hydro_random_uniform(RANDOMIZATION_SPACE_BYTES), 32);
+    const u32 random = hydro_random_uniform(RANDOMIZATION_SPACE_BYTES);
+    OSAllocFromMEM2ArenaLo(random, 32);
+    OSAllocFromMEM2ArenaHi(RANDOMIZATION_SPACE_BYTES - random, 32);
 }
