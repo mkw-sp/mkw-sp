43b576978b969cbb548baa6ace1bccd6f8aed890
MikeIsAStar <99037623+MikeIsAStar@users.noreply.github.com>
2022-10-24 15:15:00 -0400

    Typecast arguments to 'void *' when using the '%p' conversion specifier

--- payload/sp/security/Memory.c
+++ payload/sp/security/Memory.c
@@ -10,8 +10,8 @@ void Memory_ProtectRange(u32 channel, void *start, void *end, u32 permissions) {
     assert(channel <= OS_PROTECT_CHANNEL_3);
     assert(startAddress < endAddress);
 
-    OSReport("[MEM] OSProtectRange: %p - %p [CHAN: %u | PERMS: %c%c]\n", startAddress, endAddress,
-            channel, (permissions & OS_PROTECT_PERMISSION_READ) ? 'r' : '-',
+    OSReport("[MEM] OSProtectRange: %p - %p [CHAN: %u | PERMS: %c%c]\n", (void *)startAddress,
+            (void *)endAddress, channel, (permissions & OS_PROTECT_PERMISSION_READ) ? 'r' : '-',
             (permissions & OS_PROTECT_PERMISSION_WRITE) ? 'w' : '-');
     OSProtectRange(channel, startAddress, endAddress - startAddress, permissions);
 }
