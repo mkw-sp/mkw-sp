66be919985bd40a13415f388b917041fd20a3ff5
vabold <vabold820@gmail.com>
2022-11-01 13:46:49 -0400

    Remove unnecessary padding line

--- tools/directconnectioncode/directconnectioncode.py
+++ tools/directconnectioncode/directconnectioncode.py
@@ -30,7 +30,6 @@ def generate(ip: str, port: int, passcode: int):
     hex_code = passcode + port + ip
     direct_code = str(struct.unpack(">Q", hex_code)[0] + 0x3ed5142afa4755f)
 
-    direct_code = "0" * (18 - len(direct_code)) + direct_code
     direct_code = direct_code[:6] + '-' + \
         direct_code[6:12] + '-' + direct_code[-6:]
 
