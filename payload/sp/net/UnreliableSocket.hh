bf75daa592c0ef6481f7331a25bd8db8ceff0998
vabold <vabold820@gmail.com>
2022-11-09 14:52:38 -0500

    Finish UnreliableSocket

--- payload/sp/net/UnreliableSocket.hh
+++ payload/sp/net/UnreliableSocket.hh
@@ -5,28 +5,45 @@ extern "C" {
 #include <revolution.h>
 }
 
+#include <array>
 #include <optional>
 
 #include <Common.hh>
 
 namespace SP::Net {
 
+// NOTE (vabold): Please do not ask me to explain any of this
 class UnreliableSocket {
 public:
-    UnreliableSocket(hydro_kx_session_keypair keypair,
+    struct ReadInfo {
+        u16 size;
+        u8 playerIdx;
+    };
+
+    struct ConnectionInfo {
+        u32 ip;
+        u16 port;
+        hydro_kx_session_keypair keypair;
+    };
+
+    UnreliableSocket(ConnectionInfo connectionInfo,
             const char context[hydro_secretbox_CONTEXTBYTES]);
+    UnreliableSocket(std::array<ConnectionInfo, 24> &connections,
+            const char context[hydro_secretbox_CONTEXTBYTES], u32 connectionCount, u16 port);
     UnreliableSocket(UnreliableSocket &&) = delete;
     ~UnreliableSocket();
 
-    std::optional<u16> read(u8 *message, u16 maxSize);
-    bool write(const u8 *message, u16 size);
+    std::optional<ReadInfo> read(u8 *message, u16 maxSize);
+    bool write(const u8 *message, u16 size, u8 idx);
 
 private:
     bool makeNonBlocking();
 
-    hydro_kx_session_keypair m_keypair;
+    std::array<ConnectionInfo, 24> m_connections;
     char m_context[hydro_secretbox_CONTEXTBYTES];
     s32 m_handle = -1;
+    u32 m_connectionCount = 0;
+    std::optional<u16> m_port = 0;
 };
 
 } // namespace SP::Net
