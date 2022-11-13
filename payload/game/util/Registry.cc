6c825e31a16ba0389f2f36a40ce9a8d2c0df64f6
Zachruff <zachary.rubin02@gmail.com>
2022-11-13 18:51:32 -0500

    Cleaned up code.

--- payload/game/util/Registry.cc
+++ payload/game/util/Registry.cc
@@ -2,22 +2,18 @@
 
 #include "game/system/SaveManager.hh"
 
-    namespace Registry {
+namespace Registry {
 
-        const char* getItemPane(u32 itemId, u32 count) {
-            auto* saveManager = System::SaveManager::Instance();
-            auto setting = saveManager->getSetting<SP::ClientSettings::Setting::VSMegaClouds>();
+const char *getItemPane(u32 itemId, u32 count) {
+    auto *saveManager = System::SaveManager::Instance();
+    auto setting = saveManager->getSetting<SP::ClientSettings::Setting::VSMegaClouds>();
 
-            // If the item is a thundercloud, and the option for mega tcs is enabled, return the mega tc
-            // pane, otehrwise go back to original function
-            if (itemId == 0x0E) {
-                if (setting == SP::ClientSettings::VSMegaClouds::Enable) {
-                    return "mega_c";
-                }
-            }
-            
-            return REPLACED(getItemPane)(itemId, count);
-            
-        }
+    // Return mega TC pane
+    if (itemId == 0x0E && setting == SP::ClientSettings::VSMegaClouds::Enable) {
+        return "mega_c";
+    }
 
-    } // namespace Registry
+    return REPLACED(getItemPane)(itemId, count);
+}
+
+} // namespace Registry
