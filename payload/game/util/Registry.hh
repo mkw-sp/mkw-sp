bdb1783c04b3b11786b2ce1f54046ecbc065a22e
Zachruff <zachary.rubin02@gmail.com>
2022-11-19 10:33:04 -0500

    Update Registry.hh

--- payload/game/util/Registry.hh
+++ payload/game/util/Registry.hh
@@ -10,7 +10,7 @@ u32 GetCharacterMessageId(u32 characterId, bool resolveMiiNames);
 
 const char *GetCharacterPane(u32 characterId);
 
-const char *REPLACED(getItemPane)(u32 itemId, u32 count);
-REPLACE const char *getItemPane(u32 itemId, u32 count);
+const char *REPLACED(GetItemPane)(u32 itemId, u32 count);
+REPLACE const char *GetItemPane(u32 itemId, u32 count);
 
 } // namespace Registry
