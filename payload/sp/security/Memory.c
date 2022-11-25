93284dda979e2e77f999c51552d214ae67228f8c
MikeIsAStar <99037623+MikeIsAStar@users.noreply.github.com>
2022-11-25 16:10:00 -0500

    Invalidate all of the IBAT registers

--- payload/sp/security/Memory.c
+++ payload/sp/security/Memory.c
@@ -2,6 +2,21 @@
 
 #include <revolution/os.h>
 
+#define MemoryRoundDown1024B(address) (((u32)(address)) & ~0x3FF)
+#define MemoryRoundUp1024B(address) (((u32)(address) + 0x3FF) & ~0x3FF)
+
+extern void InvalidateAllIBATs(void);
+
+void Memory_InvalidateAllIBATs(void) {
+    // clang-format off
+    BOOL enabled = OSDisableInterrupts();
+    {
+        RealMode(InvalidateAllIBATs);
+    }
+    OSRestoreInterrupts(enabled);
+    // clang-format on
+}
+
 void Memory_ProtectRange(u32 channel, void *start, void *end, u32 permissions) {
     char *startAddress = (char *)MemoryRoundUp1024B(start);
     char *endAddress = (char *)MemoryRoundDown1024B(end);
