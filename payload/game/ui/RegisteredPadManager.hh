a17f0f4c4d143e3b4db7da0f3f6de3ce424d7fb6
Pablo Stebler <pablo@stebler.xyz>
2023-01-01 18:56:48 +0100

    Add a boot section setting

--- payload/game/ui/RegisteredPadManager.hh
+++ payload/game/ui/RegisteredPadManager.hh
@@ -7,6 +7,7 @@ namespace UI {
 class RegisteredPadManager {
 public:
     void setDriftIsAuto(u32 localPlayerId, bool driftIsAuto);
+    void setFlags(u32 localPlayerId, u32 flags);
 
 private:
     u8 _00[0x5c - 0x00];
---
+++ payload/sp/settings/GlobalSettings.cc
@@ -0,0 +1,48 @@
+#include "GlobalSettings.hh"
+
+#include "sp/storage/Storage.hh"
+
+#include <cstring>
+
+namespace SP::GlobalSettings {
+
+const char name[] = "Global Settings";
+
+const u32 categoryMessageIds[] = { 0 };
+
+const Entry entries[] = {
+    [static_cast<u32>(Setting::BootSection)] = {
+        .category = Category::UI,
+        .name = magic_enum::enum_name(Setting::BootSection),
+        .messageId = 0,
+        .defaultValue = 0xFFFFFFFF,
+        .valueCount = 0,
+        .valueNames = nullptr,
+        .valueMessageIds = nullptr,
+        .valueExplanationMessageIds = nullptr,
+    },
+};
+
+static Settings instance{};
+
+Settings &Instance() {
+    return instance;
+}
+
+void Init() {
+    instance.reset();
+
+    const wchar_t *path = L"/mkw-sp/settings.ini";
+
+    // TODO: Hopefully this is enough. Can always stream the file if not.
+    char iniBuffer[2048];
+    auto size = Storage::ReadFile(path, iniBuffer, sizeof(iniBuffer));
+    if (size) {
+        instance.readIni(iniBuffer, *size);
+    }
+
+    instance.writeIni(iniBuffer, sizeof(iniBuffer));
+    SP::Storage::WriteFile(path, iniBuffer, strlen(iniBuffer), true);
+}
+
+} // namespace SP::GlobalSettings
