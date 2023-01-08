5607ccfb56c7fdc18135203dbd7de57d51bfbc14
MikeIsAStar <99037623+MikeIsAStar@users.noreply.github.com>
2023-01-07 19:10:00 -0500

    Add a newline before the stack trace

--- payload/sp/Panic.c
+++ payload/sp/Panic.c
@@ -7,12 +7,13 @@
 #include <stdio.h>
 #include <string.h>
 
-// clang-format off
-void panic(const char* format, ...)
-{
-    char message_format[128];
-    snprintf(message_format, sizeof(message_format), "MKW-SP v%s\n\n" "%s\n", versionInfo.name, format);
-    // clang-format on
+void panic(const char *format, ...) {
+    char messageFormat[128];
+    snprintf(messageFormat, sizeof(messageFormat),
+            "MKW-SP v%s\n\n"
+            "%s\n\n",
+            versionInfo.name, format);
+
     char message[512];
     memset(message, 0, sizeof(message));
 
@@ -20,7 +21,7 @@ void panic(const char* format, ...)
     {
         va_list args;
         va_start(args, format);
-        size = vsnprintf(message, sizeof(message), message_format, args);
+        size = vsnprintf(message, sizeof(message), messageFormat, args);
         va_end(args);
     }
 
@@ -29,18 +30,31 @@ void panic(const char* format, ...)
     }
 
     GXColor background;
-    // clang-format off
-    switch (REGION)
-    {
-        case REGION_J: { background = GXCOLOR_RED  ; break; }
-        case REGION_E: { background = GXCOLOR_BLUE ; break; }
-        case REGION_P: { background = GXCOLOR_GREEN; break; }
-        case REGION_K: { background = GXCOLOR_PINK ; break; }
-        default      : { background = GXCOLOR_BLACK; break; }
+    switch (REGION) {
+    case REGION_J: {
+        background = GXCOLOR_RED;
+        break;
     }
-    // clang-format on
-    GXColor foreground = GXCOLOR_WHITE;
+    case REGION_E: {
+        background = GXCOLOR_BLUE;
+        break;
+    }
+    case REGION_P: {
+        background = GXCOLOR_GREEN;
+        break;
+    }
+    case REGION_K: {
+        background = GXCOLOR_PINK;
+        break;
+    }
+    default: {
+        background = GXCOLOR_BLACK;
+        break;
+    }
+    }
+
     // Impromptu luminosity calculation
+    GXColor foreground = GXCOLOR_WHITE;
     if (background.r * background.r + background.g * background.g + background.b * background.b >
             (128 * 128) * 3) {
         foreground = GXCOLOR_BLACK;
