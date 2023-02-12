922e69f21f2de7b5d9444337f59884f9f4c33c7b
MikeIsAStar <99037623+MikeIsAStar@users.noreply.github.com>
2023-02-12 02:30:30 -0500

    Do not permit the use of a modified 'StaticR.rel' file

--- payload/sp/Rel.cc
+++ payload/sp/Rel.cc
@@ -20,7 +20,86 @@ namespace SP::Rel {
 
 typedef void (*EntryFunction)(void);
 
+struct RelModuleInfo {
+    u8 hash[NET_SHA1_DIGEST_SIZE];
+    u32 size;
+};
+
 static EntryFunction entry = nullptr;
+
+static const std::array<RelModuleInfo, 4> relModuleInfoArray = {
+    // clang-format off
+    // RMCE
+    RelModuleInfo
+    {
+        {
+            0x07, 0x2B, 0xA4, 0x43, 0x82, 0xFD, 0xCA, 0x9B, 0xF5, 0xC0,
+            0xEC, 0x9D, 0x1E, 0x9B, 0x92, 0x2C, 0xAF, 0xD9, 0x92, 0x60,
+        },
+        0x004ACF94,
+    },
+    // RMCP
+    RelModuleInfo
+    {
+        {
+            0x88, 0x7B, 0xCC, 0x07, 0x67, 0x81, 0xF5, 0xB0, 0x05, 0xCC,
+            0x31, 0x7A, 0x6E, 0x3C, 0xC8, 0xFD, 0x5F, 0x91, 0x13, 0x00,
+        },
+        0x004AD3C4,
+    },
+    // RMCJ
+    RelModuleInfo
+    {
+        {
+            0x20, 0xB4, 0x16, 0x1D, 0x41, 0x5E, 0x40, 0x92, 0xA6, 0x29,
+            0x9B, 0xB2, 0x85, 0xFA, 0xA6, 0x22, 0x83, 0x5B, 0x2B, 0x16,
+        },
+        0x004ACABC,
+    },
+    // RMCK
+    RelModuleInfo
+    {
+        {
+            0xE0, 0x00, 0x62, 0x50, 0xB1, 0xE5, 0xC1, 0xBD, 0xEE, 0x8B,
+            0xDB, 0x0E, 0x78, 0x9E, 0xA2, 0x00, 0xB3, 0xA8, 0x46, 0xE4,
+        },
+        0x004AD9FC,
+    },
+    // clang-format on
+};
+
+static bool IsClean(const void *rel, u32 roundedRelSize) {
+    u32 regionIndex;
+    switch (REGION) {
+    case REGION_E:
+        regionIndex = 0;
+        break;
+    case REGION_P:
+        regionIndex = 1;
+        break;
+    case REGION_J:
+        regionIndex = 2;
+        break;
+    case REGION_K:
+        regionIndex = 3;
+        break;
+    default:
+        panic("Invalid game region!");
+    }
+
+    u32 relSize = relModuleInfoArray[regionIndex].size;
+    if (roundedRelSize < relSize) {
+        return false;
+    }
+
+    u8 relHash[NET_SHA1_DIGEST_SIZE];
+    NETSHA1Context context;
+    NETSHA1Init(&context);
+    NETSHA1Update(&context, rel, relSize);
+    NETSHA1GetDigest(&context, relHash);
+
+    return memcmp(relModuleInfoArray[regionIndex].hash, relHash, NET_SHA1_DIGEST_SIZE) == 0;
+}
 
 bool Load() {
     auto file = Storage::OpenRO("/rel/StaticR.rel");
@@ -32,6 +111,9 @@ bool Load() {
     void *src = OSGetMEM1ArenaLo();
 
     if (!file->read(src, file->size(), 0)) {
+        return false;
+    }
+    if (!IsClean(src, file->size())) {
         return false;
     }
 
@@ -69,6 +151,9 @@ bool Load() {
     return true;
 #else
     if (!file->read(Rel_getStart(), file->size(), 0)) {
+        return false;
+    }
+    if (!IsClean(Rel_getStart(), file->size())) {
         return false;
     }
 
