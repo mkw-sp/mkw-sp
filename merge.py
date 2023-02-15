e18039921e3168ddd5eed17ff9efbd4f77479f91
Gnome <david2005thomas@gmail.com>
2023-02-15 22:17:46 +0000

    Add support for filling in font value at merge time

--- merge.py
+++ merge.py
@@ -10,11 +10,17 @@ parser.add_argument('inputs', nargs = '+')
 parser.add_argument('-o', '--output')
 args = parser.parse_args()
 
-messages = {}
+messages: dict = {}
 for in_path in args.inputs:
     with open(in_path, 'r', encoding = 'utf-8') as in_file:
-        in_data = in_file.read()
-    messages = { **messages, **json5.loads(in_data) }
+        in_data = json5.load(in_file)
+
+    for message_id, message in in_data.items(): # type: ignore
+        messages[message_id] = {
+            "string": message["string"],
+            "font": message.get("font", "regular"),
+        }
+
 messages = dict(sorted(messages.items(), key = lambda item: int(item[0], 0)))
 out_data = json5.dumps(messages, ensure_ascii = False, indent = 4, quote_keys = True)
 with open(args.output, 'w', encoding = 'utf-8') as out_file:
