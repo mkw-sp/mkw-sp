41d4d5bedfbe0eab183b3e0ff28044fcb1aef9cc
Gnome! <45660393+GnomedDev@users.noreply.github.com>
2023-03-15 21:52:08 +0000

    Rewrite ut_list header in C++ (#633)

---
+++ payload/nw4r/ut/ut_list.hh
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <Common.hh>
+
+namespace nw4r::ut {
+
+//! Bidirectional list node
+struct Node {
+    void *pred;
+    void *succ;
+};
+
+class List {
+public:
+    void append(void *elem);
+    void remove(void *elem);
+private:
+    Node* m_head;
+    Node* m_tail;
+    u16 m_count;
+    u16 m_offset;
+};
+
+static_assert(sizeof(List) == 0xc);
+
+} // namespace nw4r::ut
