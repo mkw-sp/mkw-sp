dbb792ba3f30353172e4b2bc834851bc59d83ac4
Gnome! <45660393+GnomedDev@users.noreply.github.com>
2023-03-15 21:54:08 +0000

    Remove destructor override from OnlineConnectionManagerPage (#627)

--- payload/game/ui/OnlineConnectionManagerPage.hh
+++ payload/game/ui/OnlineConnectionManagerPage.hh
@@ -20,9 +20,7 @@ public:
     };
 
     OnlineConnectionManagerPage();
-    ~OnlineConnectionManagerPage() override = default;
 
-    void dt(s32 type) override;
     void onInit() override;
     void afterCalc() override;
 
