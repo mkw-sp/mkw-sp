8cdd1a7737a0a29b9e69bd8df09e19f669992296
henrymkw <86250009+henrymkw@users.noreply.github.com>
2023-03-31 21:04:06 -0500

    FIxed the dumbass changes i made earlier
    TODO: make sure a ghost doesn't write and change the button on wheel (i accidentally set it to 2 instead of a). Also change the first switch statement to be nicer.

--- payload/game/item/KartItem.cc
+++ payload/game/item/KartItem.cc
@@ -2,12 +2,17 @@
 
 #include <game/system/InputManager.hh>
 #include "game/system/SaveManager.hh"
+#include <game/system/RaceManager.hh>
+
+#include <game/util/Registry.hh>
 
 #include <sp/cs/RoomClient.hh>
 
 extern "C" {
     #include <revolution/kpad.h>
 }
+
+static bool pressedLastFrame;
 
 namespace Item {
 
@@ -22,15 +27,36 @@ void KartItem::setItem(u32 r4, u32 r5, u32 r6) {
 
 void KartItem::update() {
     REPLACED(update)();
+
     auto *saveManager = System::SaveManager::Instance();
     auto setting = saveManager->getSetting<SP::ClientSettings::Setting::ItemWheel>();
     if (setting == SP::ClientSettings::ItemWheel::Enable) {
-        auto inputManager = System::InputManager::Instance();
-        bool yPressed = inputManager->getYPressed();
-        bool yLock = inputManager->getYLock();
-        if (yPressed && !yLock) {
-            SP_LOG("Y button pressed and lock is false");
-            //inventory.currentItemCount = 7; TODO: Find a better way to keep item after using.
+        auto *playerPadProxy = System::RaceManager::Instance()->player(0)->padProxy();
+        auto buttons = playerPadProxy->currentRaceInputState().rawButtons;
+        s32 controller = playerPadProxy->pad()->getControllerId();
+        SP_LOG("buttons: %d    controller: %d", buttons, controller);
+        bool updateItem = false;
+        switch (controller) {
+            // Wanted to use Registry::Controller but getControllerId() returns s32 which i currently don't want to deal with
+        case (0): // Wii Wheel
+            updateItem = (buttons & PAD_BUTTON_A) == PAD_BUTTON_A;
+            break;
+        case (1): // Wii Chuck
+            updateItem = (buttons & WPAD_BUTTON_DOWN) == WPAD_BUTTON_DOWN;
+            break;
+        case (2): // Classic
+            updateItem = (buttons & KPAD_CL_TRIGGER_ZL) == KPAD_CL_TRIGGER_ZL;
+            break;
+        case (3): // GCN
+            updateItem = (buttons & PAD_BUTTON_Y) == PAD_BUTTON_Y;
+            break;
+        }
+        if (!updateItem) {
+            pressedLastFrame = false;
+        }
+
+        if (updateItem && !pressedLastFrame) {
+            inventory.currentItemCount = 7; //TODO: Find a better way to keep item after using.
             switch (inventory.currentItemID) {
             case (Item::TripShrooms):
                 inventory.currentItemID = Item::Star;
@@ -53,6 +79,7 @@ void KartItem::update() {
                 inventory.currentItemCount = 3;
                 break;
             }
+            pressedLastFrame = true;
         }
     }
 }
