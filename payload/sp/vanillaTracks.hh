1c1d5837e1839ab177c6d778cf7ef8db53f1059b
Gnome <david2005thomas@gmail.com>
2023-04-08 01:05:30 +0000

    Allow playing without TrackDB.ini

--- payload/sp/vanillaTracks.hh
+++ payload/sp/vanillaTracks.hh
@@ -1,9 +1,261 @@
 constexpr std::string_view vanillaManifest = R"(
 [Pack Info]
 author = Nintendo
-name = Nintendo Race Tracks
+name = Nintendo Tracks
 description = The original 32 tracks from Mario Kart Wii.
 coin = 9047f6e9b77c6a44accb46c2237609b80e459fdc,e04eec80bbe0aaf8177e0c707c05ed541fb50a23,b8312bacb25ec0f08560fd95e9edd08e821ce6cd,456d8800ee7b1b1e2d2dfe4d4654a9c3bb9bd30f,701da16b7595050ac1525579aaba40783e49cf8b,acb69b362358cdfe8a7394196696c4296acbc3a0,26776d2bd97bae6d6a8e5fa9c1fd59ae7ca5dee5,177710cd82c3019903392a77affa4615145402a3,7913710afcda69b359b5f3d614ddae8614403ef2,c49d2262b0cc65b649fe8add2daf91a3cf73cc45,
 balloon = 9047f6e9b77c6a44accb46c2237609b80e459fdc,e04eec80bbe0aaf8177e0c707c05ed541fb50a23,b8312bacb25ec0f08560fd95e9edd08e821ce6cd,456d8800ee7b1b1e2d2dfe4d4654a9c3bb9bd30f,701da16b7595050ac1525579aaba40783e49cf8b,acb69b362358cdfe8a7394196696c4296acbc3a0,26776d2bd97bae6d6a8e5fa9c1fd59ae7ca5dee5,177710cd82c3019903392a77affa4615145402a3,7913710afcda69b359b5f3d614ddae8614403ef2,c49d2262b0cc65b649fe8add2daf91a3cf73cc45,
 race = 1ae1a7d894960b38e09e7494373378d87305a163,90720a7d57a7c76e2347782f6bde5d22342fb7dd,0e380357affcfd8722329994885699d9927f8276,1896aea49617a571c66ff778d8f2abbe9e5d7479,7752bb51edbc4a95377c0a05b0e0da1503786625,e4bf364cb0c5899907585d731621ca930a4ef85c,b02ed72e00b400647bda6845be387c47d251f9d1,d1a453b43d6920a78565e65a4597e353b177abd0,72d0241c75be4a5ebd242b9d8d89b1d6fd56be8f,52f01ae3aed1e0fa4c7459a648494863e83a548c,48ebd9d64413c2b98d2b92e5efc9b15ecd76fee6,acc0883ae0ce7879c6efba20cfe5b5909bf7841b,38486c4f706395772bd988c1ac5fa30d27cae098,b13c515475d7da207dfd5badd886986147b906ff,b9821b14a89381f9c015669353cb24d7db1bb25d,ffe518915e5faaa889057c8a3d3e439868574508,8014488a60f4428eef52d01f8c5861ca9565e1ca,8c854b087417a92425110cc71e23c944d6997806,071d697c4ddb66d3b210f36c7bf878502e79845b,49514e8f74fea50e77273c0297086d67e58123e8,ba9bcfb3731a6cb17dba219a8d37ea4d52332256,e8ed31605cc7d6660691998f024eed6ba8b4a33f,bc038e163d21d9a1181b60cf90b4d03efad9e0c5,418099824af6bf1cd7f8bb44f61e3a9cc3007dae,4ec538065fdc8acf49674300cbdec5b80cc05a0d,a4bea41be83d816f793f3fad97d268f71ad99bf9,692d566b05434d8c66a55bdff486698e0fc96095,1941a29ad2e7b7bba8a29e6440c95ef5cf76b01d,077111b996e5c4f47d20ec29c2938504b53a8e76,f9a62bef04cc8f499633e4023acc7675a92771f0,b036864cf0016be0581449ef29fb52b2e58d78a4,15b303b288f4707e5d0af28367c8ce51cdeab490,
+
+[9047f6e9b77c6a44accb46c2237609b80e459fdc]
+author = Nintendo
+type = 2
+slot = 11
+id = 3409
+
+[456d8800ee7b1b1e2d2dfe4d4654a9c3bb9bd30f]
+author = Nintendo
+type = 2
+slot = 12
+id = 3413
+
+[701da16b7595050ac1525579aaba40783e49cf8b]
+author = Nintendo
+type = 2
+slot = 13
+id = 3415
+
+[e04eec80bbe0aaf8177e0c707c05ed541fb50a23]
+author = Nintendo
+type = 2
+slot = 14
+id = 3410
+
+[c49d2262b0cc65b649fe8add2daf91a3cf73cc45]
+author = Nintendo
+type = 2
+slot = 15
+id = 3420
+
+[7913710afcda69b359b5f3d614ddae8614403ef2]
+author = Nintendo
+type = 2
+slot = 21
+id = 3419
+
+[acb69b362358cdfe8a7394196696c4296acbc3a0]
+author = Nintendo
+type = 2
+slot = 22
+id = 3416
+
+[177710cd82c3019903392a77affa4615145402a3]
+author = Nintendo
+type = 2
+slot = 23
+id = 3418
+
+[26776d2bd97bae6d6a8e5fa9c1fd59ae7ca5dee5]
+author = Nintendo
+type = 2
+slot = 24
+id = 3417
+
+[b8312bacb25ec0f08560fd95e9edd08e821ce6cd]
+author = Nintendo
+type = 2
+slot = 25
+id = 3412
+
+[1ae1a7d894960b38e09e7494373378d87305a163]
+author = Nintendo
+type = 1
+slot = 11
+id = 3445
+
+[90720a7d57a7c76e2347782f6bde5d22342fb7dd]
+author = Nintendo
+type = 1
+slot = 12
+id = 3450
+
+[0e380357affcfd8722329994885699d9927f8276]
+author = Nintendo
+type = 1
+slot = 13
+id = 3454
+
+[1896aea49617a571c66ff778d8f2abbe9e5d7479]
+author = Nintendo
+type = 1
+slot = 14
+id = 3464
+
+[7752bb51edbc4a95377c0a05b0e0da1503786625]
+author = Nintendo
+type = 1
+slot = 21
+id = 3448
+
+[e4bf364cb0c5899907585d731621ca930a4ef85c]
+author = Nintendo
+type = 1
+slot = 22
+id = 3424
+
+[b02ed72e00b400647bda6845be387c47d251f9d1]
+author = Nintendo
+type = 1
+slot = 23
+id = 3426
+
+[d1a453b43d6920a78565e65a4597e353b177abd0]
+author = Nintendo
+type = 1
+slot = 24
+id = 3466
+
+[72d0241c75be4a5ebd242b9d8d89b1d6fd56be8f]
+author = Nintendo
+type = 1
+slot = 31
+id = 3431
+
+[52f01ae3aed1e0fa4c7459a648494863e83a548c]
+author = Nintendo
+type = 1
+slot = 32
+id = 3443
+
+[48ebd9d64413c2b98d2b92e5efc9b15ecd76fee6]
+author = Nintendo
+type = 1
+slot = 33
+id = 3447
+
+[acc0883ae0ce7879c6efba20cfe5b5909bf7841b]
+author = Nintendo
+type = 1
+slot = 34
+id = 3441
+
+[38486c4f706395772bd988c1ac5fa30d27cae098]
+author = Nintendo
+type = 1
+slot = 41
+id = 3433
+
+[b13c515475d7da207dfd5badd886986147b906ff]
+author = Nintendo
+type = 1
+slot = 42
+id = 3452
+
+[b9821b14a89381f9c015669353cb24d7db1bb25d]
+author = Nintendo
+type = 1
+slot = 43
+id = 3422
+
+[ffe518915e5faaa889057c8a3d3e439868574508]
+author = Nintendo
+type = 1
+slot = 44
+id = 3460
+
+[8014488a60f4428eef52d01f8c5861ca9565e1ca]
+author = Nintendo
+type = 1
+slot = 51
+id = 3439
+
+[8c854b087417a92425110cc71e23c944d6997806]
+author = Nintendo
+type = 1
+slot = 52
+id = 3430
+
+[071d697c4ddb66d3b210f36c7bf878502e79845b]
+author = Nintendo
+type = 1
+slot = 53
+id = 3462
+
+[49514e8f74fea50e77273c0297086d67e58123e8]
+author = Nintendo
+type = 1
+slot = 54
+id = 3458
+
+[ba9bcfb3731a6cb17dba219a8d37ea4d52332256]
+author = Nintendo
+type = 1
+slot = 61
+id = 3459
+
+[e8ed31605cc7d6660691998f024eed6ba8b4a33f]
+author = Nintendo
+type = 1
+slot = 62
+id = 3436
+
+[bc038e163d21d9a1181b60cf90b4d03efad9e0c5]
+author = Nintendo
+type = 1
+slot = 63
+id = 3427
+
+[418099824af6bf1cd7f8bb44f61e3a9cc3007dae]
+author = Nintendo
+type = 1
+slot = 64
+id = 3440
+
+[4ec538065fdc8acf49674300cbdec5b80cc05a0d]
+author = Nintendo
+type = 1
+slot = 71
+id = 3428
+
+[a4bea41be83d816f793f3fad97d268f71ad99bf9]
+author = Nintendo
+type = 1
+slot = 72
+id = 3435
+
+[692d566b05434d8c66a55bdff486698e0fc96095]
+author = Nintendo
+type = 1
+slot = 73
+id = 3457
+
+[1941a29ad2e7b7bba8a29e6440c95ef5cf76b01d]
+author = Nintendo
+type = 1
+slot = 74
+id = 3438
+
+[077111b996e5c4f47d20ec29c2938504b53a8e76]
+author = Nintendo
+type = 1
+slot = 81
+id = 3463
+
+[f9a62bef04cc8f499633e4023acc7675a92771f0]
+author = Nintendo
+type = 1
+slot = 82
+id = 3429
+
+[b036864cf0016be0581449ef29fb52b2e58d78a4]
+author = Nintendo
+type = 1
+slot = 83
+id = 3437
+
+[15b303b288f4707e5d0af28367c8ce51cdeab490]
+author = Nintendo
+type = 1
+slot = 84
+id = 3456
 )";
