309e8149dd4b385196f2cc48475956c4cdb795e7
Gnome <david2005thomas@gmail.com>
2023-04-09 12:15:48 +0000

    Move commands out of EGG

--- payload/egg/core/eggSystem.cc
+++ payload/egg/core/eggSystem.cc
@@ -1,40 +1,5 @@
 #include "eggSystem.hh"
 
-#include <game/ui/SectionManager.hh>
-#include <sp/Commands.h>
-#include <sp/SaveStateManager.hh>
-
-#include <cstring>
-
-sp_define_command("/ui_info", "Dump information about the UI state to the console",
-        const char *tmp) {
-    (void)tmp;
-
-    auto sectionManager = UI::SectionManager::Instance();
-    auto section = sectionManager->currentSection();
-    section->logDebuggingInfo(!strcmp(tmp, "/ui_info v"));
-}
-
-sp_define_command("/store", "", const char *tmp) {
-    (void)tmp;
-
-    if (auto saveStateManager = SP::SaveStateManager::Instance()) {
-        saveStateManager->save();
-    } else {
-        OSReport("SaveStateManager not initialized\n");
-    }
-}
-
-sp_define_command("/reload", "", const char *tmp) {
-    (void)tmp;
-
-    if (auto saveStateManager = SP::SaveStateManager::Instance()) {
-        saveStateManager->reload();
-    } else {
-        OSReport("SaveStateManager not initialized\n");
-    }
-}
-
 namespace EGG {
 
 void *TSystem::mem1ArenaLo() const {
--- payload/sp/Commands.c
+++ payload/sp/Commands.c
@@ -1,11 +1,22 @@
 #include "Commands.h"
+
+#include "sp/StringView.h"
+
+#include <game/system/SaveManager.h>
+#include <game/ui/SectionManager.h>
 #include <revolution.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 extern Command __start_commands;
 extern Command __stop_commands;
+
+const char *sOnOff[2] = {"OFF", "ON"};
+const char *fmtBool(bool b) {
+    return sOnOff[!!b];
+}
 
 // Matches "/command", "/command arg", but not "/command2"
 bool StringStartsWithCommand(const char *line, const char *cmd) {
@@ -44,6 +55,24 @@ const Command *Commands_match(const char *tmp) {
     return NULL;
 }
 
+void Commands_lineCallback(const char *buf, size_t len) {
+    StringView view = (StringView){.s = buf, .len = len};
+    const char *tmp = sv_as_cstr(view, 64);
+
+    OSReport("[SP] Line submitted: %s\n", tmp);
+
+    const Command *it = Commands_match(tmp);
+    if (it == NULL) {
+        OSReport("&aUnknown command \"%s\"\n", tmp);
+        return;
+    }
+    if (it->onBegin == NULL) {
+        OSReport("&aCommand not implemented \"%s\"\n", tmp);
+        return;
+    }
+    it->onBegin(tmp);
+}
+
 static void Commands_printHelp(size_t page) {
     const size_t numCommands = &__stop_commands - &__start_commands;
     const size_t commandsPerPage = 9;
@@ -63,4 +92,66 @@ sp_define_command("/help", "Views a list of all available commands.", const char
     int page = 1;
     sscanf(tmp, "/help %i", &page);
     Commands_printHelp(MAX(page - 1, 0));
+}
+
+sp_define_command("/example_command", "Example command", const char *tmp) {
+    (void)tmp;
+}
+
+sp_define_command("/instant_menu", "Toggle instant menu transitions", const char *tmp) {
+    (void)tmp;
+
+    if (!SaveManager_IsAvailable()) {
+        OSReport("instant_menu: Failed to load Save Manager\n");
+        return;
+    }
+    const bool menuTrans = !SaveManager_GetPageTransitions();
+    SaveManager_SetPageTransitions(menuTrans);
+    OSReport("instant_menu: Menu transition animations toggled %s\n", fmtBool(menuTrans));
+}
+
+sp_define_command("/set", "Sets a .ini setting key-value", const char *tmp) {
+    if (!SaveManager_IsAvailable()) {
+        OSReport("set: Failed to load Save Manager\n");
+        return;
+    }
+    char setting[64];
+    char value[64];
+    if (2 != sscanf(tmp, "/set %63s %63s", setting, value)) {
+        OSReport("&a/set: Invalid arguments\n");
+        return;
+    }
+    if (SaveManager_SPCurrentLicense()) {
+        OSReport("&a/set: No license active\n");
+        return;
+    }
+    SaveManager_SetSetting(setting, value);
+}
+
+sp_define_command("/section", "Transition to a certain game section", const char *tmp) {
+    if (s_sectionManager == NULL) {
+        OSReport("&aError: Section manager unavailable\n");
+        return;
+    }
+    int nextSectionId = 0;
+    if (!sscanf(tmp, "/section %i", &nextSectionId)) {
+        OSReport("&aUsage /section <id>\n");
+        return;
+    }
+
+    OSReport("&aSwitching to section %d\n", nextSectionId);
+
+    if (!SaveManager_IsAvailable()) {
+        OSReport("&aError: Save manager unavailable\n");
+        return;
+    }
+
+    // Default to license 0
+    SaveManager_SelectSPLicense(0);
+    // TODO create base license
+    SaveManager_SelectLicense(0);
+
+    SectionManager_setNextSection(s_sectionManager, __builtin_abs(nextSectionId),
+            nextSectionId < 0 ? PAGE_ANIMATION_PREV : PAGE_ANIMATION_NEXT);
+    SectionManager_startChangeSection(s_sectionManager, 5, 0xff);
 }
