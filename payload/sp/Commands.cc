584389336649bed2b39e624b48a693054f70f9c7
Gnome <david2005thomas@gmail.com>
2023-04-12 20:02:38 +0000

    Be explicit about auto pointers

--- payload/sp/Commands.cc
+++ payload/sp/Commands.cc
@@ -16,15 +16,15 @@ sp_define_command("/ui_info", "Dump information about the UI state to the consol
         const char *tmp) {
     (void)tmp;
 
-    auto sectionManager = UI::SectionManager::Instance();
-    auto section = sectionManager->currentSection();
+    auto *sectionManager = UI::SectionManager::Instance();
+    auto *section = sectionManager->currentSection();
     section->logDebuggingInfo(!strcmp(tmp, "/ui_info v"));
 }
 
 sp_define_command("/store", "", const char *tmp) {
     (void)tmp;
 
-    if (auto saveStateManager = SP::SaveStateManager::Instance()) {
+    if (auto *saveStateManager = SP::SaveStateManager::Instance()) {
         saveStateManager->save();
     } else {
         OSReport("SaveStateManager not initialized\n");
@@ -34,7 +34,7 @@ sp_define_command("/store", "", const char *tmp) {
 sp_define_command("/reload", "", const char *tmp) {
     (void)tmp;
 
-    if (auto saveStateManager = SP::SaveStateManager::Instance()) {
+    if (auto *saveStateManager = SP::SaveStateManager::Instance()) {
         saveStateManager->reload();
     } else {
         OSReport("SaveStateManager not initialized\n");
