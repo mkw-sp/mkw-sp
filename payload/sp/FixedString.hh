46b42567c60c8938f7452a44a2831385d3b251a3
riidefi <34194588+riidefi@users.noreply.github.com>
2023-04-26 14:37:21 -0500

    Implement WFixedString

--- payload/sp/FixedString.hh
+++ payload/sp/FixedString.hh
@@ -5,6 +5,10 @@
 #include <array>
 #include <string_view>
 
+extern "C" {
+#include "sp/WideUtil.h"
+}
+
 namespace SP {
 
 template <size_t N>
@@ -31,14 +35,70 @@ struct FixedString {
     constexpr bool operator==(const FixedString &) const = default;
     constexpr bool operator!=(const FixedString &) const = default;
 
-    const char *c_str() {
+    const char *c_str() const {
         // Always null terminated
         return m_buf.data();
+    }
+
+    void setUTF16(std::wstring_view view) {
+        m_len = Util_toUtf8(m_buf.data(), m_buf.size() - 1, view.data(), view.size());
+        m_buf[m_len] = '\0';
+    }
+
+    static FixedString<N> fromUTF16(std::wstring_view view) {
+        FixedString<N> result;
+        result.setUTF16(view);
+        return result;
     }
 
     size_t m_len = 0;
     // Null terminated, just in case
     std::array<char, N> m_buf = {};
+};
+
+template <size_t N>
+struct WFixedString {
+    constexpr WFixedString() = default;
+    constexpr WFixedString(const WFixedString &) = default;
+    constexpr ~WFixedString() = default;
+    constexpr WFixedString(std::wstring_view view) {
+        set(view);
+    }
+    constexpr WFixedString(const wchar_t *cstr) {
+        set(cstr);
+    }
+    constexpr operator std::wstring_view() const {
+        return std::wstring_view(m_buf.data(), m_len);
+    }
+
+    constexpr void set(std::wstring_view view) {
+        m_len = std::min(N - 1, view.size());
+        std::copy_n(view.data(), m_len, m_buf.data());
+        m_buf[m_len] = L'\0';
+    }
+
+    constexpr bool operator==(const WFixedString &) const = default;
+    constexpr bool operator!=(const WFixedString &) const = default;
+
+    const wchar_t *c_str() const {
+        // Always null terminated
+        return m_buf.data();
+    }
+
+    void setUTF8(std::string_view view) {
+        m_len = Util_toUtf16(m_buf.data(), m_buf.size() - 1, view.data(), view.size());
+        m_buf[m_len] = L'\0';
+    }
+
+    static WFixedString<N> fromUTF8(std::string_view view) {
+        WFixedString<N> result;
+        result.setUTF8(view);
+        return result;
+    }
+
+    size_t m_len = 0;
+    // Null terminated, just in case
+    std::array<wchar_t, N> m_buf = {};
 };
 
 } // namespace SP
--- payload/sp/WideUtil.h
+++ payload/sp/WideUtil.h
@@ -1,21 +1,26 @@
 #pragma once
 
 #include <Common.h>
+#include <string.h>
 #include <wchar.h>
 
-static inline void Util_toUtf16(wchar_t *dst, u32 dst_max, const char *src, u32 src_max) {
+static inline u32 Util_toUtf16(wchar_t *dst, u32 dst_max, const char *src, u32 src_max) {
     const u32 min_bound = MIN(dst_max, src_max);
     const u32 len = strnlen(src, min_bound);
 
     for (u32 i = 0; i < len; ++i) {
         dst[i] = (wchar_t)src[i];
     }
+
+    return len;
 }
-static inline void Util_toUtf8(char *dst, u32 dst_max, const wchar_t *src, u32 src_max) {
+static inline u32 Util_toUtf8(char *dst, u32 dst_max, const wchar_t *src, u32 src_max) {
     const u32 min_bound = MIN(dst_max, src_max);
     const u32 len = wcslen(src /*, min_bound*/);
 
     for (u32 i = 0; i < MIN(min_bound, len); ++i) {
         dst[i] = (char)src[i];
     }
+
+    return len;
 }
