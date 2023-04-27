1c8146a8bddb714d08ad6347e8e76bf043db04a4
Gnome <david2005thomas@gmail.com>
2023-04-27 15:28:20 +0000

    Fix sha1 build on latest devkitpro

--- vendor/sha1/sha1.c
+++ vendor/sha1/sha1.c
@@ -30,14 +30,7 @@ A million repetitions of "a"
 
 /* blk0() and blk() perform the initial expand. */
 /* I got the idea of expanding during the round function from SSLeay */
-#if BYTE_ORDER == LITTLE_ENDIAN
-#define blk0(i) (block->l[i] = (rol(block->l[i],24)&0xFF00FF00) \
-    |(rol(block->l[i],8)&0x00FF00FF))
-#elif BYTE_ORDER == BIG_ENDIAN
 #define blk0(i) block->l[i]
-#else
-#error "Endianness not defined!"
-#endif
 #define blk(i) (block->l[i&15] = rol(block->l[(i+13)&15]^block->l[(i+8)&15] \
     ^block->l[(i+2)&15]^block->l[i&15],1))
 
