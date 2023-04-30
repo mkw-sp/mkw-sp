8445e97d32c5e099fdfbe73f33fe8b7e75b31d12
Gnome <david2005thomas@gmail.com>
2023-04-30 23:49:29 +0000

    Properly do return codes

--- tools/tidy/main.py
+++ tools/tidy/main.py
@@ -1,14 +1,15 @@
-import sys
 import glob
+import itertools
 import subprocess
+import sys
 from pathlib import Path
+from typing import Literal
 
 from checks import checks
 
 
-import itertools
-if __name__ == "__main__":
-    extensions = (".py", "h", "c", "hh", "cc")
+def main() -> Literal[-1] | Literal[0]:
+    extensions = ("py", "h", "c", "hh", "cc")
     check_all = sys.argv[1] == "check_all" if len(sys.argv) > 1 else False
 
     print("Gathering files...")
@@ -24,7 +25,14 @@ if __name__ == "__main__":
         changed_raw = subprocess.check_output(["git", "diff-tree", "--no-commit-id", "--name-only", "HEAD~", "-r"]).decode()
         files = [Path(f"../../{file}") for file in changed_raw.splitlines() if file.endswith(extensions)]
 
+    failed = False
     for i, check in enumerate(checks):
         print(f"Running check {i + 1}/{len(checks)}: {check.description}")
         if (failing_file := check.run(files)) is not None:
             print(f"Failed to run check on {failing_file.name}", file=sys.stderr)
+            failed = True
+
+    return -1 if failed else 0
+
+if __name__ == "__main__":
+    sys.exit(main())
