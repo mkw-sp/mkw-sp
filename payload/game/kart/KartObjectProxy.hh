cda199d0022849a15d9d23b1722a4c52c074a5c8
GnomedDev <david2005thomas@gmail.com>
2023-05-15 11:33:06 +0100

    Remove useless Kart5c

--- payload/game/kart/KartObjectProxy.hh
+++ payload/game/kart/KartObjectProxy.hh
@@ -13,7 +13,6 @@ class KartSound;
 
 namespace Kart {
 
-class Kart5c;
 class KartAction;
 class KartBody;
 class KartCollide;
@@ -41,9 +40,7 @@ struct KartAccessor {
     KartCollide *collide;
     u8 _34[0x3c - 0x34];
     KartRollback *rollback; // Replaced
-    u8 _40[0x5c - 0x40];
-    Kart5c *unk5c;
-    u8 _44[0x64 - 0x60];
+    u8 _40[0x64 - 0x40];
 };
 
 static_assert(sizeof(KartAccessor) == 0x64);
