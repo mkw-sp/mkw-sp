54e49199322f6feb7c28f7a863e29050abaadbda
Pablo Stebler <pablo@stebler.xyz>
2023-05-21 16:50:37 +0200

    ui: Remove server mode button

--- payload/game/ui/ServicePackToolsPage.hh
+++ payload/game/ui/ServicePackToolsPage.hh
@@ -21,7 +21,6 @@ private:
     void onBack(u32 localPlayerId);
     void onStorageBenchmarkButtonFront(PushButton *button, u32 localPlayerId);
     void onThumbnailsButtonFront(PushButton *button, u32 localPlayerId);
-    void onServerModeButtonFront(PushButton *button, u32 localPlayerId);
     void onBackButtonFront(PushButton *button, u32 localPlayerId);
     void onThumbnailsNoCoursePop(MessagePage *messagePage);
 
@@ -32,12 +31,10 @@ private:
     CtrlMenuPageTitleText m_pageTitleText;
     PushButton m_storageBenchmarkButton;
     PushButton m_thumbnailsButton;
-    PushButton m_serverModeButton;
     CtrlMenuBackButton m_backButton;
     H<MultiControlInputManager> m_onBack{this, &ServicePackToolsPage::onBack};
     H<PushButton> m_onStorageBenchmarkButtonFront{this,
             &ServicePackToolsPage::onStorageBenchmarkButtonFront};
-    H<PushButton> m_onServerModeButtonFront{this, &ServicePackToolsPage::onServerModeButtonFront};
     H<PushButton> m_onThumbnailsButtonFront{this, &ServicePackToolsPage::onThumbnailsButtonFront};
     H<PushButton> m_onBackButtonFront{this, &ServicePackToolsPage::onBackButtonFront};
     H<MessagePage> m_onThumbnailsNoCoursePop{this, &ServicePackToolsPage::onThumbnailsNoCoursePop};
