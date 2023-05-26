0011162c24e531e884c864df101894e9953ecbb2
GnomedDev <david2005thomas@gmail.com>
2023-05-26 09:58:04 +0100

    Stub all ParameterFile functions

---
+++ payload/game/system/ParameterFile.cc
@@ -0,0 +1,8 @@
+#include "ParameterFile.hh"
+
+namespace System {
+
+void ParameterFile::read(EGG::Heap *) {}
+void ParameterFile::append(u32, u32, u32) {}
+
+} // namespace System
---
+++ payload/game/system/ParameterFile.hh
@@ -0,0 +1,16 @@
+#include <Common.hh>
+#include <egg/core/eggHeap.hh>
+
+namespace System {
+
+class ParameterFile {
+public:
+    ParameterFile();
+    virtual ~ParameterFile();
+    virtual void dt(s32 type);
+    REPLACE virtual void read(EGG::Heap *heap);
+
+    REPLACE void append(u32 r4, u32 r5, u32 r6);
+};
+
+} // namespace System
