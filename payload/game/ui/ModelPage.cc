d0a7ac670ab7d7223f8db2c83b65f360564ff4e3
Gnome <david2005thomas@gmail.com>
2023-06-16 00:24:43 +0100

    Add assert

--- payload/game/ui/ModelPage.cc
+++ payload/game/ui/ModelPage.cc
@@ -11,6 +11,8 @@ BackGroundModelControl &ModelPage::modelControl() {
 void ModelPage::SetModel(u32 model) {
     Section *section = SectionManager::Instance()->currentSection();
     auto *modelPage = section->page<PageId::Model>();
+    assert(modelPage);
+
     modelPage->m_modelControl.setModel(model);
 }
 
