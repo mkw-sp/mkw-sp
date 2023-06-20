99e009b248b79a1c432bd52c5c8a3d657476f9ab
GnomedDev <david2005thomas@gmail.com>
2023-06-20 11:30:20 +0100

    Add support for inline stored bmg

--- merge.py
+++ merge.py
@@ -13,13 +13,19 @@ args = parser.parse_args()
 messages: dict = {}
 for in_path in args.inputs:
     with open(in_path, 'r', encoding = 'utf-8') as in_file:
-        in_data = json5.load(in_file)
+        in_data: dict[str, str | dict] = json5.load(in_file) # type: ignore
 
-    for message_id, message in in_data.items(): # type: ignore
-        messages[message_id] = {
-            "string": message["string"],
-            "font": message.get("font", "regular"),
-        }
+    for message_id, message in in_data.items():
+        if isinstance(message, str) or message is None:
+            messages[message_id] = {
+                "string": message,
+                "font": "regular"
+            }
+        else:
+            messages[message_id] = {
+                "string": message["string"],
+                "font": message.get("font", "regular")
+            }
 
 messages = dict(sorted(messages.items(), key = lambda item: int(item[0], 0)))
 out_data = json5.dumps(messages, ensure_ascii = False, indent = 4, quote_keys = True)
