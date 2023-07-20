5d24aa4318a69702a3a8d7e1e787776aa2075dab
riidefi <34194588+riidefi@users.noreply.github.com>
2023-07-20 16:08:54 -0600

    LicenseSelectPage: Allocate enough space for all 6 potential miis

--- payload/game/ui/MiiGroup.hh
+++ payload/game/ui/MiiGroup.hh
@@ -12,7 +12,8 @@ public:
     virtual ~MiiGroup();
     virtual void dt(s32 type);
     void init(size_t count, u32 presetFlags, EGG::Heap *heap);
-    void insertFromId(size_t index, const System::MiiId *id);
+    REPLACE void insertFromId(size_t index, const System::MiiId *id);
+    void REPLACED(insertFromId)(size_t index, const System::MiiId *id);
     void insertFromRaw(u32 index, const System::RawMii *raw);
     System::Mii *get(size_t index);
     void copy(const MiiGroup *srcGroup, size_t srcIndex, size_t index);
@@ -26,7 +27,9 @@ private:
 
     System::Mii **m_miis;
     Texture *m_textures[7];
-    u8 _08[0x98 - 0x24];
+    u8 _24[0x28 - 0x24];
+    u32 m_miiCount;
+    u8 _2C[0x98 - 0x2C];
 };
 static_assert(sizeof(MiiGroup) == 0x98);
 
